c644d5e5c9053cc85fa41557946212a5c875ccc3
Sam Fonseca <sam@bypassmobile.com>
2016-03-07 04:47:41 -0600

    more config tests

--- config/config_test.go
+++ config/config_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -23,13 +24,88 @@ func TestGetFullFilePathAbsolutePath(t *testing.T) {
 func TestGetFullFilePathRelativePath(t *testing.T) {
 	cwd, _ := os.Getwd()
 	target := fmt.Sprintf("%s/config.json", cwd)
-	actual := getFullFilePath(target)
+	actual := getFullFilePath("./config.json")
 	checkResult(actual, target, t)
 }
 
 func TestGetFullFilePathStoredFilePath(t *testing.T) {
 	homeDir := os.Getenv("HOME")
-	target := fmt.Sprintf("%s/config.json", homeDir)
-	actual := getFullFilePath(target)
+	target := fmt.Sprintf("%s/.flipadelphia/config.json", homeDir)
+	actual := getFullFilePath("config.json")
 	checkResult(actual, target, t)
+}
+
+func TestReadFileContent(t *testing.T) {
+	content := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	tmpfile, err := ioutil.TempFile("", "flipadelphia")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+	actual := readConfigFile(tmpfile.Name())
+	checkResult(string(actual), string(content), t)
+}
+
+func TestParseConfigFile(t *testing.T) {
+	targetAuthUrl := "http://localhost:3005/session"
+	targetAuthHeader := "X-SESSION-TOKEN"
+	targetAuthMethod := "GET"
+	targetAuthSuccessStatusCode := "200"
+	targetDBFile := getFullFilePath("test.db")
+	targetPort := "3006"
+	content := []byte(fmt.Sprintf(`{"test": {
+    "auth_url": %q,
+    "auth_header": %q,
+    "auth_method": %q,
+    "auth_success_status_code": %q,
+    "db_file": %q,
+    "port": %q}}`, targetAuthUrl, targetAuthHeader, targetAuthMethod, targetAuthSuccessStatusCode, targetDBFile, targetPort))
+	parsedContent := parseConfigFile(content)
+	configData := parsedContent["test"]
+	checkResult(configData.AuthRequestURL, targetAuthUrl, t)
+	checkResult(configData.AuthRequestHeader, targetAuthHeader, t)
+	checkResult(configData.AuthRequestMethod, targetAuthMethod, t)
+	checkResult(configData.AuthRequestSuccessStatusCode, targetAuthSuccessStatusCode, t)
+	checkResult(configData.DBFile, targetDBFile, t)
+	checkResult(configData.ListenOnPort, targetPort, t)
+}
+
+func TestGetRuntimeEnv(t *testing.T) {
+	targetAuthUrl := "http://localhost:3005/session"
+	targetAuthHeader := "X-SESSION-TOKEN"
+	targetAuthMethod := "GET"
+	targetAuthSuccessStatusCode := "200"
+	targetDBFile := getFullFilePath("test.db")
+	targetPort := "3006"
+	content := []byte(fmt.Sprintf(`{"test": {
+    "auth_url": %q,
+    "auth_header": %q,
+    "auth_method": %q,
+    "auth_success_status_code": %q,
+    "db_file": %q,
+    "port": %q}}`, targetAuthUrl, targetAuthHeader, targetAuthMethod, targetAuthSuccessStatusCode, targetDBFile, targetPort))
+	tmpfile, err := ioutil.TempFile("", "flipadelphia")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+	configData := getRuntimeEnv(tmpfile.Name(), "test")
+	checkResult(configData.AuthRequestURL, targetAuthUrl, t)
+	checkResult(configData.AuthRequestHeader, targetAuthHeader, t)
+	checkResult(configData.AuthRequestMethod, targetAuthMethod, t)
+	checkResult(configData.AuthRequestSuccessStatusCode, targetAuthSuccessStatusCode, t)
+	checkResult(configData.DBFile, targetDBFile, t)
+	checkResult(configData.ListenOnPort, targetPort, t)
 }
